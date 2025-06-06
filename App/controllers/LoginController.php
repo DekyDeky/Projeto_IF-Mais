af0dd6cc4f60363c819e05a7435f88e54f89650b
DekyDeky <dekyifprtb@gmail.com>
2025-06-06 14:47:36 -0300

    feat: Estrutura de Pastas

---
+++ App/controllers/LoginController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\controllers;
+
+class LoginController {
+    public function mostrarLogin() {
+        include __DIR__ . '/../views/login.php';
+    }
+}
---
+++ Routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use App\controllers\LoginController;
+
+//Pega o caminho da URL
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+//Auxilia ao mudar a pasta destino
+$base = '/Projeto_IF-Mais/Public';
+$route = str_replace($base, '', $url);
+
+switch ($route) {
+    case '/login':
+        (new LoginController())->mostrarLogin();
+        break;
+    default:
+        http_response_code(404);
+        echo "Página não encontrada";
+        echo "$url";
+};
+
+?>
